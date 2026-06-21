40d6968a5738b07bb79cd99219219b7e0f038a11
Manuel Martin Morante <manum@tinybird.co>
2026-06-20 20:20:44 -0700

    bump versio

--- src/boltzmann_generators/__init__.py
+++ src/boltzmann_generators/__init__.py
@@ -2,7 +2,7 @@
 
 from . import analysis, base, energies, flows, io, losses, mcmc, sampling, services, train, training
 
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 __all__ = [
     "analysis",
     "base",
